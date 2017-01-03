d6c0ee6d05fe4f57539f574f1462bc091242e4b0
Jozef Marko <jomo.android2@gmail.com>
2017-01-03 09:06:50 +0100

    Results 17

--- Programming/Learning/PCA_SVM/configuration_default.py
+++ Programming/Learning/PCA_SVM/configuration_default.py
@@ -1,10 +1,10 @@
 
 
-NUM_OF_COMPONENTS = 20
+NUM_OF_COMPONENTS = 150
 PARAM_GRID = {'C': [1, 3, 5, 8, 10],
               'gamma': [0.0001, 0.0005, 0.001, 0.005, 0.01, 0.1],
               'kernel': ['linear', 'rbf', 'poly', 'sigmoid']}
-PARAM_GRID = {'C': [10, 20, 30, 50, 70, 100, 120],
+PARAM_GRID = {'C': [10, 25, 50, 120, 230, 450],
               'gamma': [0.005, 0.0075, 0.01],
               'kernel': ['rbf']}
 USE_TEST_DATA = False
