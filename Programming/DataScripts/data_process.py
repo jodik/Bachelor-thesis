f65ad1a157f0ddd307b082942808e9c63c53ce21
Jozef Marko <jomo.android2@gmail.com>
2017-01-06 06:48:10 -0500

    Results 18

--- Programming/DataScripts/data_process.py
+++ Programming/DataScripts/data_process.py
@@ -18,53 +18,59 @@ def filterAndCreateTrainSet(validation_names, test_names, full_data):
     return train_data
 
 
-def getPermutation(permutation_index, labels, validation_size, test_size):
+def choose_subset(to_skip, to_choose, total_count, perm, labels):
+    for i in range(len(to_skip)):
+        s = to_skip[i] + to_choose[i]
+        if s > total_count[i]:
+            d = total_count[i] - s
+            to_skip[i] -= d
+    subset = np.zeros(0, dtype=int)
+    rest = np.zeros(0, dtype=int)
+    for t in perm:
+        if to_choose[labels[t]] > 0 and to_skip[labels[t]] == 0:
+            label = labels[t]
+            to_choose[label] -= 1
+            total_count[label] -= 1
+            subset = np.append(subset, t)
+        else:
+            to_skip[labels[t]] -= 1
+            rest = np.append(rest, t)
+    return subset, rest
+
+
+def getPermutation(permutation_index, labels, test_size):
     num_of_images_total = len(labels)
     if permutation_index >= 10 or permutation_index < 0:
         raise ValueError('Permutation index should not be larger than 9 and lower than 0')
     perm = np.arange(num_of_images_total)
-    for i in range(permutation_index + 1):
-        np.random.shuffle(perm)
+    np.random.seed(conf.SEED)
+    np.random.shuffle(perm)
 
-    percentage = validation_size / float(num_of_images_total)
-    counts = np.array(percentage * np.bincount(np.array(labels, dtype=int)), dtype=int)
-    to_add = validation_size - sum(counts)
+    total_count = np.bincount(np.array(labels, dtype=int))
+    percentage = test_size / float(num_of_images_total)
+    counts = np.array(percentage * total_count, dtype=int)
+    to_add = test_size - sum(counts)
     for i in np.arange(counts.shape[0])[:to_add]:
         counts[i] += 1
-    a = np.zeros(0, dtype=int)
-    b = np.zeros(0, dtype=int)
-    c = np.zeros(0, dtype=int)
-    d = np.zeros(0, dtype=int)
-    counts_tmp = np.copy(counts)
-    for t in perm:
-        if counts[labels[t]] > 0:
-            counts[labels[t]] -= 1
-            a = np.append(a, t)
-        else:
-            b = np.append(b, t)
-    counts = counts_tmp
-    print(counts)
-    for t in b:
-        if counts[labels[t]] > 0:
-            counts[labels[t]] -= 1
-            c = np.append(c, t)
-        else:
-            d = np.append(d, t)
 
-    print (np.append(a, np.append(c, d)))
-    return np.append(a, np.append(c, d))
+    print(total_count)
+    test_set, perm = choose_subset(np.zeros(len(counts)), np.copy(counts), total_count, perm, labels)
+
+    validation_percentage = (float(1)/conf.CROSS_VALIDATION_ITERATIONS)
+    counts = np.array(validation_percentage * total_count, dtype=int)
+    validation_set, perm = choose_subset(np.copy(counts) * permutation_index, np.copy(counts), total_count, perm, labels)
+
+    return perm, validation_set, test_set
 
 
 def process(full_data, permutation_index):
     original_set_size = full_data.get_original_data_set_size()
     TEST_SIZE = int(original_set_size * (conf.TEST_PERCENTAGE / 100.0))
-    VALIDATION_SIZE = int(original_set_size * (conf.VALIDATION_PERCENTAGE / 100.0))
 
-    perm = getPermutation(permutation_index, full_data.labels[:original_set_size], VALIDATION_SIZE, TEST_SIZE)
+    train_perm, val_perm, test_perm = getPermutation(permutation_index, full_data.labels[:original_set_size], TEST_SIZE)
 
-    original_data = copy.deepcopy(full_data).apply_permutation(perm)
-    test_data = original_data.create_data_set(0, TEST_SIZE)
-    validation_data = original_data.create_data_set(TEST_SIZE, TEST_SIZE + VALIDATION_SIZE)
+    test_data = copy.deepcopy(full_data).apply_permutation(test_perm)
+    validation_data = copy.deepcopy(full_data).apply_permutation(val_perm)
     train_data = filterAndCreateTrainSet(validation_data.names, test_data.names, full_data)
 
     data_sets = DataSets(train_data, validation_data, test_data)
